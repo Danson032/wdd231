39cefa9d36d3c37841fe42384543c5d8207e47f8
Danson Nganga <dansonnganga019@gmail.com>
2026-03-04 23:32:41 +0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-const hamButton=document.getElementById('ham-btn');
-
+const hamButton = document.getElementById('ham-btn');
+const navButton = document.getElementById('nav');
 hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('show');
+    navButton.classList.toggle('show');
 })
