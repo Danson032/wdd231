3f29b91cf4405f069727a8788fb26faa5ca199f3
Danson Nganga <dansonnganga019@gmail.com>
2026-03-04 15:06:49 +0300

    commit

--- week1_code_along/scripts/site.js
+++ week1_code_along/scripts/site.js
@@ -1,6 +1,8 @@
 //store the selected element that we are going to use
 const navButton = document.getElementById(`ham-btn`)
+const navLinks = document.getElementById(`nav-bar`)
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('show');
+  navLinks.classList.toggle('show');
 });
