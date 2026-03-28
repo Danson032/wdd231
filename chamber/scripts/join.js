d406eb64172e416baf7779f67cc56b037d8fa100
Danson Nganga <dansonnganga019@gmail.com>
2026-03-29 01:38:10 +0300

    commit

---
+++ chamber/scripts/join.js
@@ -0,0 +1,39 @@
+//menu btn//
+const hamBtn = document.getElementById("ham-btn");
+const nav = document.getElementById("nav");
+const hamLabel = document.getElementById("ham-label");
+
+if (hamBtn && nav) {
+    hamBtn.addEventListener("click", () => {
+        nav.classList.toggle("show");
+        hamBtn.classList.toggle("show");
+
+        if (hamBtn.classList.contains("show")) {
+            hamLabel.textContent = "Close";
+        } else {
+            hamLabel.textContent = "Menu";
+        }
+    });
+}
+
+//footer
+document.getElementById("lastModified").textContent =
+    "Last Updated: " + document.lastModified;
+
+document.getElementById("copyright").textContent =
+    "© " + new Date().getFullYear() + " Nairobi Chamber";
+
+// timestamp
+const timestampField = document.getElementById("timestamp");
+if (timestampField) {
+  timestampField.value = new Date().toISOString();
+}
+
+// modals
+function openModal(id) {
+  document.getElementById(id).showModal();
+}
+
+function closeModal(id) {
+  document.getElementById(id).close();
+}
